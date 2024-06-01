cb80cceb20d97e9fff4ee9c71063b225d4ce4085
tushar48 <tusharchamp777@gmail.com>
2024-06-01 15:08:02 +0530

    Added 10th file

---
+++ Day10.py
@@ -0,0 +1,31 @@
+#if-else statement
+
+a = int(input("Enter your age"))
+# a = 19
+
+if a > 18:
+    print("you can drive")
+    print("voila")
+else:
+    print("cant drive my man")
+
+ 
+print(1==1)
+print(1 < 1)
+print(1 > 1)
+print(1 <= 1)
+print(1 >= 1)
+print(1 != 1)
+
+
+if a < 10:
+    print("value is 10")
+elif a > 10 and a <= 15:
+    print("value between 10 and 15")
+
+elif a > 15 and a < 20:
+    print("value between 15 and 20")
+else:
+    print("value is large")
+
+
