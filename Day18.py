2afda4b72a04bc09ee523bc2b83e20fd3501d8f3
tushar48 <tusharchamp777@gmail.com>
2024-06-08 16:28:32 +0530

    Added 18 file

---
+++ Day18.py
@@ -0,0 +1,19 @@
+#List in Python
+
+#List is an ordered collection of items
+
+l = [1,2,3]
+
+print(l)
+print(type(l))
+
+print(l[0])
+
+l.append([1,2,4])
+print(l)
+
+l.extend([1,2,4])
+print(l)
+
+
+
